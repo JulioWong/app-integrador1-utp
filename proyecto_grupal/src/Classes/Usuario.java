d75a545e3ddc37ca1637dc8d723a85fcbae864a9
Julio Wong <julioo.wong@gmail.com>
2020-11-16 15:48:47 -0500

    commit

--- proyecto_grupal/src/Classes/Usuario.java
+++ proyecto_grupal/src/Classes/Usuario.java
@@ -1,14 +1,26 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Classes;
 
-/**
- *
- * @author julio.wong
- */
+import Data.Database;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+
 public class Usuario {
+    private final Facultad facultad;
+    private final String usuario;
+    private final String contrasena;
+    private final Database database;
+
+    public Usuario(Facultad facultad, String usuario, String contrasena) {
+        this.database = new Database();
+        this.facultad = facultad;
+        this.usuario = usuario;
+        this.contrasena = contrasena;
+    }
     
+    public Boolean login() {
+        Document mydoc = this.database.getMongoCollection("user").find(
+                eq("user", this.usuario)).first();
+        System.out.println(mydoc);
+        return false;
+    }
 }
---
+++ proyecto_grupal/src/Data/Database.java
@@ -0,0 +1,36 @@
+package Data;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
+
+public class Database {
+    private final MongoClientURI uri;
+    private final String user;
+    private final String password;
+    private final String database;
+    private final MongoClient mongoClient;
+    private final MongoDatabase mongoDatabase;
+
+    public Database() {
+        this.database = "integrador1";
+        this.user = "jwong";
+        this.password = "integrador1";
+        this.uri = new MongoClientURI(
+            "mongodb://" + this.user + ":" + this.password + "@" +
+            "cluster0-shard-00-00.aptoz.mongodb.net:27017," +
+            "cluster0-shard-00-01.aptoz.mongodb.net:27017," +
+            "cluster0-shard-00-02.aptoz.mongodb.net:27017/" + this.database +
+            "?ssl=true&replicaSet=atlas-xyxo8p-shard-0&" +
+            "authSource=admin&retryWrites=true&w=majority"
+        );
+        this.mongoClient = new MongoClient(this.uri);
+        this.mongoDatabase = mongoClient.getDatabase(this.database);
+    }
+    
+    public MongoCollection<Document> getMongoCollection(String table) {
+        return this.mongoDatabase.getCollection(table);
+    }
+}
