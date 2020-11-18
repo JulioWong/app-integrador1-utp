2af73bcf678cd284c477a3c26dee4f0401cf82c4
Julio Wong <julioo.wong@gmail.com>
2020-11-18 10:49:26 -0500

    refactoring

--- proyecto_grupal/src/Classes/Usuario.java
+++ proyecto_grupal/src/Classes/Usuario.java
@@ -1,33 +1,44 @@
 package Classes;
 
-import Data.UsuarioModel;
+import Data.Database;
+import com.mongodb.client.model.Filters;
+import static com.mongodb.client.model.Filters.eq;
 import org.apache.commons.codec.digest.DigestUtils;
 import org.bson.Document;
 
 public class Usuario {
+    private final Database database;
+    private final String collection;
+    
     private int userId;
-    private final Facultad facultad;
     private final String usuario;
     private final String contrasena;
-    private final UsuarioModel usuarioModel;
 
-    public Usuario(Facultad facultad, String usuario, String contrasena) {
-        this.facultad = facultad;
+    public Usuario(String usuario, String contrasena) {
         this.usuario = usuario;
         this.contrasena = DigestUtils.md5Hex(contrasena);
-        this.usuarioModel = new UsuarioModel();
+        this.database = new Database();
+        this.collection = "user";
     }
 
     public int getUserId() {
         return userId;
     }
     
+    private Document getUserByLogin(String usuario, String password) {
+        return this.database.getMongoCollection(this.collection)
+            .find(Filters.and(
+                eq("user", usuario), 
+                eq("password", password)
+            )
+        ).first();
+    }
+    
     public Boolean login() {
         try {
-            Document usuario = this.usuarioModel.getUserByLogin(
-            facultad.getFacultadId(), this.usuario, this.contrasena);
-            if (!usuario.get("userId").toString().equals("")) {
-                this.userId = Integer.parseInt(usuario.get("userId").toString());
+            Document data = this.getUserByLogin(this.usuario, this.contrasena);
+            if (!data.get("userId").toString().equals("")) {
+                this.userId = Integer.parseInt(data.get("userId").toString());
                 return true;
             }
             return false;
--- proyecto_grupal/src/Views/Equipos/formSearch.java
+++ proyecto_grupal/src/Views/Equipos/formSearch.java
@@ -30,6 +30,7 @@ public class formSearch extends javax.swing.JFrame {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
+        jLabel2 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
@@ -44,15 +45,23 @@ public class formSearch extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jLabel2.setText("jLabel2");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 635, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel2)
+                .addContainerGap(569, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 313, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(jLabel2)
+                .addContainerGap(257, Short.MAX_VALUE))
         );
 
         jTabbedPane1.addTab("Información", jPanel1);
@@ -132,7 +141,7 @@ public class formSearch extends javax.swing.JFrame {
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 36, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jTabbedPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 347, Short.MAX_VALUE)
+                .addComponent(jTabbedPane1)
                 .addContainerGap())
         );
 
@@ -186,6 +195,7 @@ public class formSearch extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JMenu jMenu1;
     private javax.swing.JMenu jMenu2;
     private javax.swing.JMenu jMenu3;
